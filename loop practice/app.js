7f84627da35786d2cebbef12c9bb88cdf5baecdf
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-06-03 15:53:47 +0845

    Add files via upload

---
+++ loop practice/app.js
@@ -0,0 +1,34 @@
+// var cityToCheck = prompt("bfunsfnf")
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// // if (cityToCheck === cleanestCities[0]) {
+// //     alert("It's one of the cleanest cities");
+// //     }
+// //     else if (cityToCheck === cleanestCities[1]) {
+// // alert("It's one of the cleanest cities");
+// //     }
+// //     else if (cityToCheck === cleanestCities[2]) {
+// //     alert("It's one of the cleanest cities");
+// //     }
+// //     else if (cityToCheck === cleanestCities[3]) {
+// //     alert("It's one of the cleanest cities");
+// //      }
+// // else if (cityToCheck === cleanestCities[4]) {
+// //     alert("It's one of the cleanest cities");
+// //     };
+//     for (var i = 0; i <= cleanestCities.length; i++) {
+//         if (cityToCheck === cleanestCities[i]) {
+//         alert("It's one of the cleanest cities");
+//         }
+//     }
+// var matchFound = "";
+//  for (var i = 0; i <= 4; i++){
+// var matchFound = "no";
+//  if (cityToCheck === cleanestCities[i]) {
+//  matchFound = "yes";
+//  alert("It's one of the cleanest cities");
+//  break;
+//  }
+//  if (matchFound === "no") {
+// alert("It's not on the list");
+// break;
+//  }}
