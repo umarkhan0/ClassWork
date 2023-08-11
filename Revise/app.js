5cdbee82cf8dd9161aabca4027efff4c5b9444d7
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-08-11 23:19:52 +0500

    Add files via upload

---
+++ Revise/app.js
@@ -0,0 +1,66 @@
+// alert("hello")
+// var name = "areeba";
+// var ummar ;
+// ummar ="umar";
+// var name  = "areeba"
+// var alare  = alert(name)
+// var areeba_ngg = 10
+// var whatsLeftOver = 10 - 3;
+// whatsLeftOver--
+// areeba_ngg--
+// var num = 1;
+//  var newNum = --num;
+// var second = '20'
+// var message = "Thanks for your input! 1";
+//  var messages = "Thanks for your input! 2";
+
+// console.log(alare)
+// var value = Number(prompt("enter your"))
+// var contru = prompt()
+
+// var message =  Number(prompt())
+// var messages = value + contru
+// console.log( value , contru ,  message)
+// console.log(value +  message)
+// console.log(value -  message)
+var gender = prompt("gender");
+
+// if (x === "male") 
+//  alert("ah jaho");
+//  }
+//  var correctAns = "pakistan";
+// if (x >= 18) {
+//     if (x < 51) {
+//         alert("ah jaho");
+//     } else {
+//         alert("sorry")
+//     }
+// } else {
+//     alert("sorry")
+// }
+
+
+
+// if(x >= 18){
+//     alert("eahjhu")
+// }
+// else if(gender == "male"){
+//     alert("hi man")
+// }
+// else{
+//     alert("sorrt")
+// }
+
+
+if(gender === 'female'){
+var x = +prompt("age");
+    if(x >= 18){
+        alert("welcome")
+    }
+    else{
+        alert("apki age kam ha")
+    }
+ }
+ else{
+    alert("sorry")
+ }
