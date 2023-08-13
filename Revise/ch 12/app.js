5b6a209a94a80a1aee48ecfc1d5cd5fd40289548
Umar khan <125694026+Umarkhan0@users.noreply.github.com>
2023-08-13 22:49:39 +0500

    Add files via upload

---
+++ Revise/ch 12/app.js
@@ -0,0 +1,44 @@
+// var age = prompt("Enter your age");
+// var natnality = prompt("enter your contry");
+// var qulification = prompt("enter your qulification")
+// if(age == 18 && natnality == "pakistan" && qulification == "matric"){
+//     alert("wellcome");
+// }else{
+//     alert("no entery")
+// }
+// var weight = 400;
+// var time  = 4
+// var  gender = 'male'
+// if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+
+// var weight = 400;
+// var time  = 4
+// var  gender = 'male'
+// var age = 18
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+
+
+// var weight = 400;
+// var time  = 4;
+// if(weight > 500 || time < 3){
+//     alert("wellcome")
+// }
+// var res  = "umar"
+// var  gender = 'male'
+// var age = 60
+// if (age > 55 || gender == 'male' && res != "U.S.") {
+//     alert("wellcome")
+// }\
+// var arr = [res,"khan","areeba","khan" , 2432 , true ];
+// arr.pop()
+// console.log(arr[5])
